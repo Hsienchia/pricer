5eedb40821d1e6fb01352fcc9c3ab6804304cebf
Sakiyoshi <huangxianjia@gmail.com>
2018-02-22 20:29:54 +0800

    Implement the Box-Muller transformation to generate Gaussian random variables

--- SimpleMCMain1.cpp
+++ SimpleMCMain1.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <cmath>
-//#include<random1.h> 
+#include "random.h"
 using namespace std;
 
-double SimpleMonteCarlo1(double Expiry)
+double SimpleMonteCarlo1(double expiry, double strike, double spot, double vol, double r, unsigned long NumberOfPaths)
+{
+    double variance = vol * vol * expiry;
+    double movedSpot = spot * exp(r * expiry - 0.5 * variance);
+    double runningSum = 0;
+    double curSpot = 0;
+
+    for (unsigned int i = 0; i < NumberOfPaths; ++i)
+    {
+        double GaussianRV = randn();
+        curSpot = movedSpot * exp(sqrt(variance) * GaussianRV);
+        double curPayoff = (curSpot > strike) ? curSpot - strike : 0;
+        runningSum += curPayoff;
+    }
+
+    double meanPayoff = runningSum / NumberOfPaths;
+    meanPayoff *= exp(-r * expiry);
+    return meanPayoff;
+}
