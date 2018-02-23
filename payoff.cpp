b4dfdc71808645bfd610888c1200d65367d55f8f
Sakiyoshi <huangxianjia@gmail.com>
2018-02-23 11:15:33 +0800

    The class payoff

--- payoff.cpp
+++ payoff.cpp
@@ -1,5 +1,5 @@
 #include "payoff.h"
-#include <MinMax.h>
+#include <algorithm>
 
 payoff::payoff(double _strike, OptionType _type) : strike(_strike), type(_type) {}
 
@@ -8,9 +8,9 @@ double payoff::operator()(double spot) const
     switch (type)
     {
     case call:
-        return max(spot - strike, 0.0);
+        return std::max(spot - strike, 0.0);
     case put:
-        return max(strike - spot, 0.0);
+        return std::max(strike - spot, 0.0);
     default:
         throw("Unknown option type found.");
     }
